38d63854c884e8184e98275e196a90f9a45943b1
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-11-04 09:09:41 +0100

    index comments

--- index.py
+++ index.py
@@ -31,11 +31,18 @@ class Window(QtWidgets.QWidget):
     self.setWindowTitle("Excel")
     self.setGeometry(500, 500, 2000, 1000)
 
+    # A flag if all autosaving features shall be inited. 
+    # At this point there is nothing changing this.
+    self.autoSave = True
 
-
-    
+    # Create the base Boxlayout and attach it ti self
     vBoxLayout = QVBoxLayout()
+    self.setLayout(vBoxLayout)
 
+
+    # This gets called when there is a change detected originating from the file
+    # The idea was to rerender the complete table. But sadly I get some Multithreading errors
+    # and the vBoxLayout#replaceWidget method does not work like thought. 
     def onFileChange():
       print("file originating change")
 
@@ -45,16 +52,28 @@ class Window(QtWidgets.QWidget):
       self._createTable()
       table2 = self.table
       vBoxLayout.replaceWidget(table1, table2)
-
-        
-    self.poi = UPOI("data/test", onFileChange)
     
-    self.setLayout(vBoxLayout)
+    # Only really use it when the autoSave flag is set
+    useOnFileChanged = None
+    if self.autoSave:
+      useOnFileChanged = onFileChange
 
-    self.autoSave = True
+    
+    # Declare and init a UPOI instance which is connected to a specified file.
+    # In the future this hardcoded string could be replace by the context in which 
+    # this app was called (like double click on a file)
+    self.poi = UPOI("data/test", useOnFileChanged)
+    
+    
+    
+
+    # Create the main view table with the current UPOI as init data. 
     self._createTable()
     vBoxLayout.addWidget(self.table)
 
+
+    # Declare and Init other GUI elements
+    # Add row and add col Buttons are inited and appended properly here
     addRowButton = QPushButton("Add row")
     addRowButton.clicked.connect(lambda : 
       self.addRow()
@@ -72,30 +91,52 @@ class Window(QtWidgets.QWidget):
     botBox.addWidget(addRowButton)
     botBox.addWidget(addColButton)
 
-    
+  # Gets called when a row wants to be added by the user
   def addRow(self): 
+    # Change the gui table accordingly
     self.table.setRowCount(self.table.rowCount() + 1)
+    # Change poi accordingly
     self.poi.data.append(Object())
+    # Save to file when autosave flag is set
+    if (self.autoSave): 
+      self.poi.save()
     
-
+  # Gets called when a col wants to be added by the user
+  # Here a column name must be inquired
   def addCol(self): 
+    # Gets called when the inquiry is successfully finished by the user
     def then(txt): 
+      # get needed data (the col count)
       count = self.table.columnCount()
+      # Change the gui table accordingly
       self.table.setColumnCount(count + 1)
       self.table.setHorizontalHeaderItem(count, QTableWidgetItem(txt))
+      # Change poi accordingly
       self.poi.types.append(txt)
-      self.poi.save()
 
+      # Save to file when autosave flag is set
+      if (self.autoSave): 
+        self.poi.save()
+
+    # Validator to ensure no column names are added twice
+    # This gets called very often, thus we must ensure that 
+    # it is very fast.
     def validator(s: str):
       return s not in self.poi.types
+
+    # Ask the user for the col name. When the user cancels the then callback will not be called
     inquire("Column name", validator, self).then(then)
 
+  # This gets called every time a table cell gets changed by the user
   def tableChanged(self, x, y): 
+      # Set the according attr into the parsed poi object.
     setattr(self.poi.data[x], self.poi.types[y], self.table.item(x, y).text())
+
+    # Save to file when autosave flag is set
     if (self.autoSave): 
       self.poi.save()
 
-
+  # Just a helper function to create a new table with the current poi object.
   def _createTable(self): 
     poi = self.poi
     
@@ -105,6 +146,7 @@ class Window(QtWidgets.QWidget):
     table.setColumnCount(len(poi.types))
     table.setRowCount(len(poi.data))
 
+    # Set header
     for i in range(len(poi.types)):
       table.setHorizontalHeaderItem(i, QTableWidgetItem(poi.types[i]))
 
@@ -113,6 +155,7 @@ class Window(QtWidgets.QWidget):
 
     data = poi.data
 
+    # Set cells
     for x in range(len(data)):
       for y in range(len(poi.types)):
         s = getattr(data[x], poi.types[y])
@@ -120,7 +163,7 @@ class Window(QtWidgets.QWidget):
         table.setItem(x, y, q)
 
 
-
+    # Call table changed when table cell changes
     table.cellChanged.connect(lambda x, y: 
       self.tableChanged(x, y)
     )
