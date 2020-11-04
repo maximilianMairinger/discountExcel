afa1118e6edd0ee57efd6e5dc231c0a7b83e01f3
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-11-04 08:46:18 +0100

    some fixes

--- serialize.py
+++ serialize.py
@@ -13,12 +13,12 @@ class FsHandler(FileSystemEventHandler):
 
   def on_deleted(self, event):
     # print("del")
-    if self.filePath == event.src_path:
+    if self.filePath == os.path.abspath(event.src_path):
       self.updateCallback("")
 
   def on_modified(self, event):
-    # print("mod")
-    if self.filePath == event.src_path:
+    print("mod",self.filePath, event.src_path )
+    if self.filePath == os.path.abspath(event.src_path):
       content = open(self.filePath, "r").read()
       if self.lastFileContent != content:
         self.lastFileContent = content
@@ -26,7 +26,7 @@ class FsHandler(FileSystemEventHandler):
 
   def on_moved(self, event):
     # print("mov")
-    if self.filePath == event.src_path:
+    if self.filePath == os.path.abspath(event.src_path):
       print("event.dest_path")
       print(event.dest_path)
       print("event.dest_path")
@@ -43,7 +43,7 @@ class Serialize:
     self.filePath(filePath, True)
     self.ending(ending, True)
     if onChange != None: 
-      self.enableLiveFileObserver()
+      self.enableLiveFileObserver(onChange)
   
   def ending(self, ending = None, dontWrite = False): 
     if ending == None: 
@@ -54,16 +54,24 @@ class Serialize:
       self._ending = ending
       self._changeFilePath(self.filePath(), ending, dontWrite)
 
-  def enableLiveFileObserver(self, onChange):
+
+  def enableLiveFileObserver(self, onChange = None):
     if self._observer == None: 
-      def onChange(content):
+      if onChange == None:
+        if hasattr(self, "lastOnChange"):
+          onChange = self.lastOnChange
+        else:
+          return
+      self.lastOnChange = onChange
+      def change(content):
+        print("new")
         nonlocal onChange
         onChange(content)
       
       def onFileMoved(to):
         self.filePath(to)
 
-      event_handler = FsHandler(self._path, onChange, onFileMoved)
+      event_handler = FsHandler(self._path, change, onFileMoved)
       self._observer = observer = Observer()
       observer.schedule(event_handler, os.path.dirname(os.path.realpath(self._path)), True)
       observer.start()
@@ -98,7 +106,7 @@ class Serialize:
       if (not dontWrite):
         read = self.read()
 
-      self._path = os.path.normpath(to)
+      self._path = os.path.abspath(to)
 
       if (not dontWrite):
         self.write(read)
@@ -110,5 +118,14 @@ class Serialize:
 
 
   def write(self, data):
-    return open(self._path, "w").write(data)
+    observerActive = self._observer != None
+    if observerActive:
+      self.disableLiveFileObserver()
+
+    ret = open(self._path, "w").write(data)
+
+    if observerActive:
+      self.enableLiveFileObserver()
+
+    return ret
 
